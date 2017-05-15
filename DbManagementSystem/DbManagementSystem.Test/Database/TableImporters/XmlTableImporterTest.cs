51aac55dc9f447ae78ad4fab82db1a2a87ded256
Elena Creangă <elena.creanga.mail@gmail.com>
2017-05-15 19:44:02 +0300

    Add XMLTableImporterTest

---
+++ DbManagementSystem/DbManagementSystem.Test/Database/TableImporters/XmlTableImporterTest.cs
@@ -0,0 +1,37 @@
+﻿using DbManagementSystem.Core.Database;
+using DbManagementSystem.Core.Database.TableImporters;
+using NSubstitute;
+using Xunit;
+
+namespace DbManagementSystem.Test.Database.TableImporters
+{
+    public class XmlTableImporterTest
+    {
+        public XmlTableImporter GetSUT()
+        {
+            return new XmlTableImporter();
+        }
+
+        [Fact]
+        public void Import_WhenValidData_ShouldReturnTrue()
+        {
+            var xmlTableImporter = GetSUT();
+            var databaseConnection = Substitute.For<IDatabaseConnection>();
+            var data = "<Data><Row>Line1</Row><Row>Line2</Row></Data>";
+            var table = "testTable";
+            var result = xmlTableImporter.Import(databaseConnection, table, data);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Import_WhenInvalidFormatData_ShouldReturnFalse()
+        {
+            var xmlTableImporter = GetSUT();
+            var databaseConnection = Substitute.For<IDatabaseConnection>();
+            var data = "<Data><Row>Line1<Row>Line2</Row></Data>";
+            var table = "testTable";
+            var result = xmlTableImporter.Import(databaseConnection, table, data);
+            Assert.False(result);
+        }
+    }
+}
