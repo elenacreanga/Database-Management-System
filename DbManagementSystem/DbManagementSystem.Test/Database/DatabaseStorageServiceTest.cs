0b002994be402b5a14bb252eafb8f66559151d52
Elena Creangă <elena.creanga.mail@gmail.com>
2017-05-17 01:27:58 +0300

    Add more database storage service tests

--- DbManagementSystem/DbManagementSystem.Test/Database/DatabaseStorageServiceTest.cs
+++ DbManagementSystem/DbManagementSystem.Test/Database/DatabaseStorageServiceTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using DbManagementSystem.Core.Database;
 using Xunit;
@@ -20,7 +21,7 @@ namespace DbManagementSystem.Test.Database
         {
             var databaseStorageService = GetSUT();
             const string newdb = "*$£$%&£$DHasd";
-            Assert.ThrowsAny<Exception>(() => databaseStorageService.CreateDatabase(newdb));
+            Assert.ThrowsAny<ArgumentException>(() => databaseStorageService.CreateDatabase(newdb));
         }
 
         [Fact]
@@ -61,7 +62,7 @@ namespace DbManagementSystem.Test.Database
             var databaseStorageService = GetSUT();
             const string databaseName = "*$£$%&£$DHasd";
 
-            Assert.ThrowsAny<Exception>(() => databaseStorageService.DeleteDatabase(databaseName));
+            Assert.ThrowsAny<ArgumentException>(() => databaseStorageService.DeleteDatabase(databaseName));
         }
 
         [Fact]
@@ -70,7 +71,7 @@ namespace DbManagementSystem.Test.Database
             var databaseStorageService = GetSUT();
             const string databaseName = "C:\\database";
 
-            Assert.ThrowsAny<Exception>(() => databaseStorageService.DeleteDatabase(databaseName));
+            Assert.ThrowsAny<DirectoryNotFoundException>(() => databaseStorageService.DeleteDatabase(databaseName));
         }
 
         [Fact]
@@ -159,7 +160,7 @@ namespace DbManagementSystem.Test.Database
             var databaseStorageService = GetSUT();
             const string serverlocationDatabase = "C:\\inexistentServerLocation";
 
-            Assert.ThrowsAny<Exception>(() => databaseStorageService.GetDatabases(serverlocationDatabase));
+            Assert.ThrowsAny<DirectoryNotFoundException>(() => databaseStorageService.GetDatabases(serverlocationDatabase));
         }
 
         [Fact]
@@ -190,7 +191,7 @@ namespace DbManagementSystem.Test.Database
                     CreateTempDatabase(databaseStorageService, serverlocationDatabase);
                     CreateTempDatabase(databaseStorageService, alreadyExistingServerLocationDatabase);
 
-                    Assert.ThrowsAny<Exception>(() => databaseStorageService.MoveDatabase(serverlocationDatabase,
+                    Assert.ThrowsAny<IOException>(() => databaseStorageService.MoveDatabase(serverlocationDatabase,
                         alreadyExistingServerLocationDatabase));
                 },
                 () =>
@@ -206,7 +207,7 @@ namespace DbManagementSystem.Test.Database
             var databaseStorageService = GetSUT();
             const string serverlocationDatabase = "C:\\serverLocation";
             const string newServerLocationDatabase = "C:\\newServerLocation";
-            Assert.ThrowsAny<Exception>(() => databaseStorageService.MoveDatabase(serverlocationDatabase,
+            Assert.ThrowsAny<DirectoryNotFoundException>(() => databaseStorageService.MoveDatabase(serverlocationDatabase,
                 newServerLocationDatabase));
         }
 
@@ -219,7 +220,7 @@ namespace DbManagementSystem.Test.Database
             tempDbServer.ExecuteOperation(() =>
                 {
                     CreateTempDatabase(databaseStorageService, serverlocationDatabase);
-                    Assert.ThrowsAny<Exception>(() => databaseStorageService.MoveDatabase(serverlocationDatabase,
+                    Assert.ThrowsAny<IOException>(() => databaseStorageService.MoveDatabase(serverlocationDatabase,
                         newServerLocationDatabase));
                 },
                 () =>
@@ -238,7 +239,7 @@ namespace DbManagementSystem.Test.Database
             tempDbServer.ExecuteOperation(() =>
                 {
                     CreateTempDatabase(databaseStorageService, serverlocationDatabase);
-                    Assert.ThrowsAny<Exception>(() => databaseStorageService.MoveDatabase(serverlocationDatabase,
+                    Assert.ThrowsAny<ArgumentException>(() => databaseStorageService.MoveDatabase(serverlocationDatabase,
                         newServerLocationDatabase));
                 },
                 () =>
@@ -248,11 +249,228 @@ namespace DbManagementSystem.Test.Database
                 });
         }
 
+        [Fact]
+        public void CreateTable_WhenTableDoesNotExist_ShouldCreate()
+        {
+            var databaseStorageService = GetSUT();
+            const string serverlocationDatabase = "C:\\serverLocation";
+            tempDbServer.ExecuteOperation(() =>
+            {
+                CreateTempDatabase(databaseStorageService, serverlocationDatabase);
+                const string tablePath = serverlocationDatabase + "\\" + "table";
+                databaseStorageService.WriteAllText(tablePath, "");
+                var result = databaseStorageService.ExistsTable(tablePath);
+                Assert.True(result);
+            },
+            () => { RevertDatabasesCreation(databaseStorageService, new []{ serverlocationDatabase });});
+        }
+
+        [Fact]
+        public void CreateTable_WhenDatabaseDoesNotExist_ShouldFail()
+        {
+            var databaseStorageService = GetSUT();
+            const string serverlocationDatabase = "C:\\serverLocation";
+            const string tablePath = serverlocationDatabase + "\\" + "table";
+            Assert.ThrowsAny<Exception>(() => databaseStorageService.WriteAllText(tablePath, string.Empty));
+        }
+
+        [Fact]
+        public void CreateTable_WhenTableAlreadyExists_ShouldThrowError()
+        {
+            var databaseStorageService = GetSUT();
+            const string serverlocationDatabase = "C:\\serverLocation";
+            tempDbServer.ExecuteOperation(() =>
+                {
+                    CreateTempDatabase(databaseStorageService, serverlocationDatabase);
+                    const string tablePath = serverlocationDatabase + "\\" + "table";
+                    databaseStorageService.WriteAllText(tablePath, string.Empty);
+                    var result = databaseStorageService.ExistsTable(tablePath);
+                    Assert.True(result);
+                    Assert.ThrowsAny<Exception>(() => databaseStorageService.WriteAllText(tablePath, string.Empty));
+                },
+                () => { RevertDatabasesCreation(databaseStorageService, new[] { serverlocationDatabase }); });
+        }
+
+        [Fact]
+        public void AlterTable_WhenTableExists_ShouldSucceed()
+        {
+            var databaseStorageService = GetSUT();
+            const string serverlocationDatabase = "C:\\serverLocation";
+            const string columns = "nume, prenume";
+            tempDbServer.ExecuteOperation(() =>
+                {
+                    CreateTempDatabase(databaseStorageService, serverlocationDatabase);
+                    const string tablePath = serverlocationDatabase + "\\" + "table";
+                    databaseStorageService.WriteAllText(tablePath, string.Empty);
+                    var isTableCreated = databaseStorageService.ExistsTable(tablePath);
+                    Assert.True(isTableCreated);
+                    databaseStorageService.WriteAllLines(tablePath, new [] {columns});
+                    var result = databaseStorageService.ReadAllLines(tablePath);
+                    Assert.Equal(columns, result.FirstOrDefault());
+
+                },
+                () => { RevertDatabasesCreation(databaseStorageService, new[] { serverlocationDatabase }); });
+        }
+
+        [Fact]
+        public void AlterTable_WhenTableDoesNotExist_ShouldCreateIt()
+        {
+            var databaseStorageService = GetSUT();
+            const string serverlocationDatabase = "C:\\serverLocation";
+            const string columns = "nume, prenume";
+            tempDbServer.ExecuteOperation(() =>
+                {
+                    CreateTempDatabase(databaseStorageService, serverlocationDatabase);
+                    const string tablePath = serverlocationDatabase + "\\" + "table";
+                    var isTableCreated = databaseStorageService.ExistsTable(tablePath);
+                    Assert.False(isTableCreated);
+                    databaseStorageService.WriteAllLines(tablePath, new [] {columns});
+                    var result = databaseStorageService.ReadAllLines(tablePath);
+                    Assert.Equal(columns, result.FirstOrDefault());
+
+                },
+                () => { RevertDatabasesCreation(databaseStorageService, new[] { serverlocationDatabase }); });
+        }
+
+        [Fact]
+        public void DeleteTable_WhenTableExists_ShouldSucceed()
+        {
+            var databaseStorageService = GetSUT();
+            const string serverlocationDatabase = "C:\\serverLocation";
+            tempDbServer.ExecuteOperation(() =>
+                {
+                    CreateTempDatabase(databaseStorageService, serverlocationDatabase);
+                    const string tablePath = serverlocationDatabase + "\\" + "table";
+                    databaseStorageService.WriteAllText(tablePath, string.Empty);
+                    var isTableCreated = databaseStorageService.ExistsTable(tablePath);
+                    Assert.True(isTableCreated);
+
+                    databaseStorageService.DeleteTable(tablePath);
+                    var result = databaseStorageService.ExistsTable(tablePath);
+
+                    Assert.False(result);
+
+                },
+                () => { RevertDatabasesCreation(databaseStorageService, new[] { serverlocationDatabase }); });
+        }
+
+        [Fact]
+        public void DeleteTable_WhenTableDoesNotExist_ShouldThrowError()
+        {
+            var databaseStorageService = GetSUT();
+            const string serverlocationDatabase = "C:\\serverLocation";
+            tempDbServer.ExecuteOperation(() =>
+                {
+                    CreateTempDatabase(databaseStorageService, serverlocationDatabase);
+                    const string tablePath = serverlocationDatabase + "\\" + "table";
+                    var isTableCreated = databaseStorageService.ExistsTable(tablePath);
+                    Assert.False(isTableCreated);
+
+                    Assert.ThrowsAny<Exception>(() => databaseStorageService.DeleteTable(tablePath));
+                },
+                () => { RevertDatabasesCreation(databaseStorageService, new[] { serverlocationDatabase }); });
+        }
+
+        [Fact]
+        public void DeleteTable_WhenInvalidPathForTable_ShouldThrowError()
+        {
+            var databaseStorageService = GetSUT();
+            const string serverlocationDatabase = "C:\\serverLocation";
+            tempDbServer.ExecuteOperation(() =>
+                {
+                    CreateTempDatabase(databaseStorageService, serverlocationDatabase);
+                    const string tablePath = serverlocationDatabase + "\\" + "*342#@$%table";
+                    var isTableCreated = databaseStorageService.ExistsTable(tablePath);
+                    Assert.False(isTableCreated);
+
+                    Assert.ThrowsAny<ArgumentException>(() => databaseStorageService.DeleteTable(tablePath));
+                },
+                () => { RevertDatabasesCreation(databaseStorageService, new[] { serverlocationDatabase }); });
+        }
+
+        [Fact]
+        public void ReadAllLines_WhenPathCorrect_ShouldSucceed()
+        {
+            var databaseStorageService = GetSUT();
+            const string serverlocationDatabase = "C:\\serverLocation";
+            tempDbServer.ExecuteOperation(() =>
+                {
+                    CreateTempDatabase(databaseStorageService, serverlocationDatabase);
+                    const string tablePath = serverlocationDatabase + "\\" + "table";
+                    const string randomtext = "RandomText";
+                    CreateTempTable(databaseStorageService, tablePath, randomtext);
+
+                    var result = databaseStorageService.ReadAllLines(tablePath);
+                    Assert.NotNull(result);
+                    Assert.NotEmpty(result);
+                    Assert.Equal(1, result.Length);
+                    Assert.Equal(randomtext, result.FirstOrDefault());
+                },
+                () => { RevertDatabasesCreation(databaseStorageService, new[] { serverlocationDatabase }); });
+        }
+
+        [Fact]
+        public void ReadAllLines_WhenInvalidPath_ShouldThrowError()
+        {
+            var databaseStorageService = GetSUT();
+            const string serverlocationDatabase = "C:\\serverLocation";
+            tempDbServer.ExecuteOperation(() =>
+                {
+                    CreateTempDatabase(databaseStorageService, serverlocationDatabase);
+                    const string tablePath = serverlocationDatabase + "\\" + "table";
+                    var isTableCreated = databaseStorageService.ExistsTable(tablePath);
+                    Assert.False(isTableCreated);
+
+                    Assert.ThrowsAny<FileNotFoundException>(() => databaseStorageService.ReadAllLines(tablePath));
+                },
+                () => { RevertDatabasesCreation(databaseStorageService, new[] { serverlocationDatabase }); });
+        }
+
+        [Fact]
+        public void EnumerateTables_WhenValidPath_ShouldReturnTables()
+        {
+            var databaseStorageService = GetSUT();
+            const string databaseName = "C:\\tempServerLocation";
+            const string tableOne = "tableOne";
+            const string tableNameOne = databaseName + "\\" + tableOne;
+            const string tableTwo = "tableTwo";
+            const string tableNameTwo = databaseName + "\\" + tableTwo;
+
+            tempDbServer.ExecuteOperation(() =>
+                {
+                    CreateTempDatabase(databaseStorageService, databaseName);
+                    CreateTempTable(databaseStorageService, tableNameOne);
+                    CreateTempTable(databaseStorageService, tableNameTwo);
+                    var result = databaseStorageService.EnumerateTables(databaseName);
+                    Assert.NotNull(result);
+                    Assert.NotEmpty(result);
+                    Assert.Equal(2, result.Count());
+                    Assert.Contains(tableNameOne, result);
+                    Assert.Contains(tableNameTwo, result);
+                },
+                () => { RevertDatabasesCreation(databaseStorageService, new[] { databaseName }); });
+        }
+
+        [Fact]
+        public void EnumerateTables_WhenInvalidPath_ShouldThrowError()
+        {
+            var databaseStorageService = GetSUT();
+            const string databaseName = "C:\\tempServerLocation";
+            Assert.ThrowsAny<DirectoryNotFoundException>(() => databaseStorageService.EnumerateTables(databaseName));
+        }
+
         private static void CreateTempDatabase(DatabaseStorageService databaseStorageService, string serverlocationDatabase)
         {
             databaseStorageService.CreateDatabase(serverlocationDatabase);
             var isDatabaseSuccessfullyCreated = databaseStorageService.ExistsDatabase(serverlocationDatabase);
             Assert.True(isDatabaseSuccessfullyCreated);
+        }
+
+        private static void CreateTempTable(DatabaseStorageService databaseStorageService, string tablePath, string tableContent = null)
+        {
+            databaseStorageService.WriteAllText(tablePath, tableContent);
+            var isTableCreated = databaseStorageService.ExistsTable(tablePath);
+            Assert.True(isTableCreated);
         }
 
         private static void RevertDatabasesCreation(DatabaseStorageService databaseStorageService,
