2437c015279ff80013799df86f9af38c5467842a
Elena Creangă <elena.creanga.mail@gmail.com>
2017-05-16 23:49:10 +0300

    Add database manager test

--- DbManagementSystem/DbManagementSystem.Test/Database/DatabaseManagerTest.cs
+++ DbManagementSystem/DbManagementSystem.Test/Database/DatabaseManagerTest.cs
@@ -1,6 +1,30 @@
-﻿namespace DbManagementSystem.Test.Database
+﻿using DbManagementSystem.Core.Database;
+using NSubstitute;
+using Xunit;
+
+namespace DbManagementSystem.Test.Database
 {
-    class DatabaseManagerTest
+    public class DatabaseManagerTest
     {
+        [Fact]
+        public void GetDatabase_WhenNoDatabase_ShouldReturnNull()
+        {
+            var databaseConnection = Substitute.For<IDatabaseConnection>();
+
+            var databaseStorage = new DatabaseStorageService();
+            var databaseConfiguration = new DatabaseCofiguration(databaseStorage);
+
+            databaseConnection.GetDatabaseConfiguration().Returns(databaseConfiguration);
+
+            var databaseManager = GetSUT(databaseConnection);
+            var result = databaseManager.GetDatabase("nonExistingDb");
+            Assert.Null(result);
+        }
+
+        private DatabaseManager GetSUT(IDatabaseConnection databaseConnection)
+        {
+            return new DatabaseManager(databaseConnection);
+        }
+
     }
 }
