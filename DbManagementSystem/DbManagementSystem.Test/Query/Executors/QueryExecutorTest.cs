6d2d0c2fd18604ea130ceca41c3455baa900db7b
Elena Creangă <elena.creanga.mail@gmail.com>
2017-05-18 11:13:28 +0300

    Add tests to query executor

--- DbManagementSystem/DbManagementSystem.Test/Query/Executors/QueryExecutorTest.cs
+++ DbManagementSystem/DbManagementSystem.Test/Query/Executors/QueryExecutorTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using DbManagementSystem.Core.Database;
 using DbManagementSystem.Core.Query;
 using DbManagementSystem.Core.Query.Executors;
@@ -46,7 +47,7 @@ namespace DbManagementSystem.Test.Query.Executors
         }
 
         [Fact]
-        public void Execute_WithValidQuery_ShouldSucceed()
+        public void Execute_WithValidCreateDbQuery_ShouldSucceed()
         {
             var queryExecutor = GetSUT();
             var databaseConnection = Substitute.For<IDatabaseConnection>();
@@ -64,6 +65,143 @@ namespace DbManagementSystem.Test.Query.Executors
             var result = queryExecutor.Execute(databaseConnection, query);
             Assert.True(result.Success);
             Assert.Equal("Database successfully created", result.Message);
+        }
+
+        [Fact]
+        public void Execute_WithValidSelectQuery_ShouldSucceed()
+        {
+            var queryExecutor = GetSUT();
+            var databaseConnection = Substitute.For<IDatabaseConnection>();
+            var databaseStorage = Substitute.For<IDatabaseStorageService>();
+            var databaseConfiguration = Substitute.For<IDatabaseConfiguration>();
+
+            databaseStorage.ExistsDatabase(Arg.Any<string>()).Returns(true);
+            const string serverLocation = "server-location";
+            const string databaseName = "test";
+            const string tableName = "test";
+
+            databaseStorage.ExistsDatabase(serverLocation + "/" + databaseName).Returns(true);
+            databaseConnection.GetServerLocation().Returns(serverLocation);
+            databaseConnection.GetDatabaseConfiguration().Returns(databaseConfiguration);
+            databaseConfiguration.DatabaseStorageService.Returns(databaseStorage);
+            databaseConnection.GetDatabaseName().Returns(databaseName);
+            databaseStorage.ExistsTable(Arg.Any<string>()).Returns(true);
+            databaseStorage.ReadAllLines(Arg.Any<string>()).Returns(new []{"nume:string/r/nIon/r/nVasile"});
+            IQuery query = new SqlQuery("SELECT * FROM " + tableName);
+            var result = queryExecutor.Execute(databaseConnection, query);
+            Assert.True(result.Success);
+            Assert.Equal("Query successfully executed", result.Message);
+        }
+
+        [Fact]
+        public void Execute_WithInvalidSelectQuery_ShouldFail()
+        {
+            var queryExecutor = GetSUT();
+            var databaseConnection = Substitute.For<IDatabaseConnection>();
+            var databaseStorage = Substitute.For<IDatabaseStorageService>();
+            var databaseConfiguration = Substitute.For<IDatabaseConfiguration>();
+
+            databaseStorage.ExistsDatabase(Arg.Any<string>()).Returns(true);
+            const string serverLocation = "server-location";
+            const string databaseName = "test";
+            const string tableName = "test";
+
+            databaseStorage.ExistsDatabase(serverLocation + "/" + databaseName).Returns(true);
+            databaseConnection.GetServerLocation().Returns(serverLocation);
+            databaseConnection.GetDatabaseConfiguration().Returns(databaseConfiguration);
+            databaseConfiguration.DatabaseStorageService.Returns(databaseStorage);
+            databaseConnection.GetDatabaseName().Returns(databaseName);
+            databaseStorage.ExistsTable(Arg.Any<string>()).Returns(true);
+            databaseStorage.ReadAllLines(Arg.Any<string>()).Returns(new[] { "nume:string/r/nIon/r/nVasile" });
+            IQuery query = new SqlQuery("SELECT** * FROM " + tableName);
+            var result = queryExecutor.Execute(databaseConnection, query);
+            Assert.False(result.Success);
+            Assert.Equal("Invalid query!", result.Message);
+        }
+
+        [Fact]
+        public void Execute_WithValidDeleteQuery_ShouldSucceed()
+        {
+            var queryExecutor = GetSUT();
+            var databaseConnection = Substitute.For<IDatabaseConnection>();
+            var databaseStorage = Substitute.For<IDatabaseStorageService>();
+            var databaseConfiguration = Substitute.For<IDatabaseConfiguration>();
+
+            databaseStorage.ExistsDatabase(Arg.Any<string>()).Returns(true);
+            const string serverLocation = "server-location";
+            const string databaseName = "test";
+            const string tableName = "test";
+
+            databaseStorage.ExistsDatabase(serverLocation + "/" + databaseName).Returns(true);
+            databaseConnection.GetServerLocation().Returns(serverLocation);
+            databaseConnection.GetDatabaseConfiguration().Returns(databaseConfiguration);
+            databaseConfiguration.DatabaseStorageService.Returns(databaseStorage);
+            databaseConnection.GetDatabaseName().Returns(databaseName);
+            databaseStorage.ExistsTable(Arg.Any<string>()).Returns(true);
+            databaseStorage.ReadAllLines(Arg.Any<string>()).Returns(new[] { "nume:string","Ion" });
+            IQuery query = new SqlQuery("DELETE FROM " + tableName + " WHERE nume='Ion'");
+            var result = queryExecutor.Execute(databaseConnection, query);
+            Assert.True(result.Success);
+            Assert.Equal("Successfully deleted 1 records.", result.Message);
+        }
+
+        [Fact]
+        public void Execute_WithInvalidDeleteQuery_ShouldFail()
+        {
+            var queryExecutor = GetSUT();
+            var databaseConnection = Substitute.For<IDatabaseConnection>();
+            var databaseStorage = Substitute.For<IDatabaseStorageService>();
+            var databaseConfiguration = Substitute.For<IDatabaseConfiguration>();
+
+            databaseStorage.ExistsDatabase(Arg.Any<string>()).Returns(true);
+            const string serverLocation = "server-location";
+            const string databaseName = "test";
+            const string tableName = "test";
+
+            databaseStorage.ExistsDatabase(serverLocation + "/" + databaseName).Returns(true);
+            databaseConnection.GetServerLocation().Returns(serverLocation);
+            databaseConnection.GetDatabaseConfiguration().Returns(databaseConfiguration);
+            databaseConfiguration.DatabaseStorageService.Returns(databaseStorage);
+            databaseConnection.GetDatabaseName().Returns(databaseName);
+            databaseStorage.ExistsTable(Arg.Any<string>()).Returns(true);
+            databaseStorage.ReadAllLines(Arg.Any<string>()).Returns(new[] { "nume:string","Ion" });
+            databaseConfiguration.ParseDataTypeValue("string", "Ion").Returns("Ion");
+            databaseConfiguration.ParseDataTypeValue("string", "Vasile").Returns("Vasile");
+
+            var allowedDataTypes = new List<string> { "=" };
+            databaseConfiguration.GetAllowedOperators().Returns(allowedDataTypes);
+            databaseConfiguration.IsOperatorAllowedForDataType("string", "=").Returns(true);
+            databaseConfiguration.PerformOperation(Arg.Any<string>(), Arg.Any<object>(), Arg.Any<object>(), Arg.Any<string>()).Returns(false);
+            IQuery query = new SqlQuery("DELETE FROM " + tableName + " WHERE nume='Vasile'");
+            var result = queryExecutor.Execute(databaseConnection, query);
+            Assert.True(result.Success);
+            Assert.Equal("Successfully deleted 0 records.", result.Message);
+        }
+
+        [Fact]
+        public void Execute_WithValidUpdateQuery_ShouldSucceed()
+        {
+            var queryExecutor = GetSUT();
+            var databaseConnection = Substitute.For<IDatabaseConnection>();
+            var databaseStorage = Substitute.For<IDatabaseStorageService>();
+            var databaseConfiguration = Substitute.For<IDatabaseConfiguration>();
+
+            databaseStorage.ExistsDatabase(Arg.Any<string>()).Returns(true);
+            const string serverLocation = "server-location";
+            const string databaseName = "test";
+            const string tableName = "test";
+
+            databaseStorage.ExistsDatabase(serverLocation + "/" + databaseName).Returns(true);
+            databaseConnection.GetServerLocation().Returns(serverLocation);
+            databaseConnection.GetDatabaseConfiguration().Returns(databaseConfiguration);
+            databaseConfiguration.DatabaseStorageService.Returns(databaseStorage);
+            databaseConnection.GetDatabaseName().Returns(databaseName);
+            databaseStorage.ExistsTable(Arg.Any<string>()).Returns(true);
+            databaseStorage.ReadAllLines(Arg.Any<string>()).Returns(new[] { "nume:string","Ion" });
+            IQuery query = new SqlQuery("UPDATE " + tableName + " SET nume='Vasile' WHERE nume='Ion'");
+            var result = queryExecutor.Execute(databaseConnection, query);
+            Assert.True(result.Success);
+            Assert.Equal("Query successfully executed", result.Message);
         }
 
         [Fact]
