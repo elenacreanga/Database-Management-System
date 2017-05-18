5bf09b2b10b86495737831bd0ae47cd37ab63b5f
Elena Creangă <elena.creanga.mail@gmail.com>
2017-05-18 05:17:26 +0300

    Add XMLQueryResultSerializer Tests

--- DbManagementSystem/DbManagementSystem.Test/Query/QueryResultSerializers/XmlQueryResultSerializerTest.cs
+++ DbManagementSystem/DbManagementSystem.Test/Query/QueryResultSerializers/XmlQueryResultSerializerTest.cs
@@ -1,6 +1,32 @@
-﻿namespace DbManagementSystem.Test.Query.QueryResultSerializers
+﻿using System.Collections.Generic;
+using DbManagementSystem.Core.Query;
+using DbManagementSystem.Core.Query.QueryResultSerializers;
+using Xunit;
+
+namespace DbManagementSystem.Test.Query.QueryResultSerializers
 {
-    class XmlQueryResultSerializerTest
+    public class XmlQueryResultSerializerTest
     {
+        [Fact]
+        public void Serialize()
+        {
+            var xmlQueryResultSerializer = GetSUT();
+            var queryResult = new List<Dictionary<string, object>>();
+            const string rowData = "Gigel";
+            const string column = "Nume";
+            var record = new Dictionary<string, object> { [column] = rowData };
+
+            queryResult.Add(record);
+
+            IQueryResult sqlQueryResult = new SqlQueryResult(1, true, "Success", queryResult);
+            var result = xmlQueryResultSerializer.Serialize(sqlQueryResult);
+            var expectedResult = "<Data>\n<Row><Nume>Gigel</Nume></Row></Data>";
+            Assert.Equal(expectedResult, result);
+        }
+
+        private XmlQueryResultSerializer GetSUT()
+        {
+            return new XmlQueryResultSerializer();
+        }
     }
 }
